d735d505b6eb78d1e3fba428c0510c5cd0e1609a
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2023-09-24 17:05:09 +0900

    feat: フォントをnext/font経由で使う方式に移行(#8)

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,10 +11,6 @@ class MyDocument extends Document {
             href="https://fonts.gstatic.com"
             crossOrigin=""
           />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,500;0,600;0,700;1,500;1,600;1,700&display=swap"
-            rel="stylesheet"
-          />
           <link rel="icon" href="/favicon.ico" />
         </Head>
         <body>
--- src/styles/globals.ts
+++ src/styles/globals.ts
@@ -1,5 +1,10 @@
 import { css } from '@emotion/react';
 import 'modern-css-reset/dist/reset.min.css';
+import { Montserrat } from 'next/font/google';
+
+const montserrat = Montserrat({
+  subsets: ['latin'],
+});
 
 export const globalStyle = css`
   html,
@@ -9,6 +14,8 @@ export const globalStyle = css`
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
       Ubuntu, Cantarell, 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
     background-color: #f2f2f2;
+
+    ${montserrat.style}
   }
 
   a {
